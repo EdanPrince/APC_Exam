89c295258c083949d9a8410b3c603e50942da2c3
EdanPrince <112059492+EdanPrince@users.noreply.github.com>
2023-05-31 09:50:27 +0700

    Add files via upload

---
+++ Animal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger(100, "tiger");
+            Lion lion = new Lion(200, "lion");
+
+            tiger.Show();
+            lion.Show();                  
+        }
+    }
+}
